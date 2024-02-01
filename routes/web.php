188b793ef0d7c69aeb851554fff08501e59ae6c4
DiuMasum <masum35-577@diu.edu.bd>
2024-02-01 13:09:09 +0600

    model in laravel and how to create it

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DemoController;
 use App\Http\Controllers\RegistrationController;
+use App\Models\Customer;
+
 
 Route::get('/register', [RegistrationController::class, 'index']);
 Route::post('/register', [RegistrationController::class, 'register']);
+
+Route::get('/customer', function(){
+    $customers = Customer::all();
+    echo "<pre>";
+    print_r($customers->toArray());
+});
